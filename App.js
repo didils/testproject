12f0629d21ef5a840200fa797f84e40d0858c9c6
Home <Home@seolssaui-MacBook-Pro.local>
2018-09-02 22:02:48 +0900

    persist gate is added

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
 import React from 'react';
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import configureStore from "./redux/configureStore";
+
+const { persistor, store } = configureStore();
 
 export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  }
   render() {
+    const { isLoadingComplete } = this.state;
+    if(!isLoadingComplete){
+      return(
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <View style={styles.container}>
+            <Text>제발 좀 되자!</Text>
+          </View>
+        </PersistGate>
+      </Provider>
     );
   }
+  _loadAssetsAsync = async() => {
+    return Promise.all([
+      Asset.loadAsync([
+        require("./assets/images/icon.png"),
+        require("./assets/images/splash.png")
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ]);
+  };
+  _handleLoadingError = error => {
+    console.error(error);
+  }
+  _handleFinishLoading = async() => {
+    this.setState({
+      isLoadingComplete: true
+    })
+  };
 }
 
 const styles = StyleSheet.create({
